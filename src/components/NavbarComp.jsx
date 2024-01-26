651c1d7538c84bebc92e3066f458dcb9fa1bcd4b
adityaapraveen <adityapraveen18@gmail.com>
2024-01-26 22:07:28 +0530

    work on explore dropdown

--- src/components/NavbarComp.jsx
+++ src/components/NavbarComp.jsx
@@ -1,24 +1,35 @@
 import React from 'react'
 import './NavbarStyles.css'
+import { FaSearch, FaBell  } from "react-icons/fa";
+import { CgProfile } from "react-icons/cg";
+import { SlCalender } from "react-icons/sl";
+import { IoMdArrowDropdown } from "react-icons/io";
+
 
 const NavbarComp = () => {
   return (
     <div className='navbar'>
         <div className="first-two">
             <span className='shaale-logo'>shaale</span>
-
-            <span>Explore dropdown</span>
+            <div className="explore-dropdown">
+                <span>Explore<IoMdArrowDropdown /></span>
+                <ul>
+                    <li>Music</li>
+                    <li>Dance</li>
+                </ul>
+            </div>
         </div>
         <div className="last-four">
             <div className="search-box">
                 <input type='text' placeholder='search' />
-                <img src="C:\Users\adity\OneDrive\Desktop\Shaale Clone\shaale-test\src\icon\assets\searchicon-w.png" alt="" />
+                <FaSearch className='search-icon'/>
+
             </div>
         <div className="icons">
             <ul>
-                <li>cal</li>
-                <li>remi</li>
-                <li>profile</li>
+                <li><SlCalender /></li>
+                <li><FaBell /></li>
+                <li><CgProfile /></li>
             </ul>
         </div>
         </div>
