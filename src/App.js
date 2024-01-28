45215fa45848e2d32112252fb50a5198d532959a
adityaapraveen <adityapraveen18@gmail.com>
2024-01-28 20:10:20 +0530

    carousel not working

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import NavbarComp from './components/NavbarComp';
+import { CarouselDemo } from './components/CarouselDemo';
 
 function App() {
   return (
     <div className="App">
       <NavbarComp />
+      <CarouselDemo />
     </div>
   );
 }
